80cc551f04bcc22879a91a927aabad21b2c07daf
Bhanu Kumar <bhanukumar.boinidi@gmail.com>
2024-10-27 11:37:43 +0530

    mongodb edited

--- functions/availability.js
+++ functions/availability.js
@@ -2,10 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const Availability = require('../models/Availability');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 exports.handler = async (event) => {
   const { httpMethod, path, queryStringParameters, pathParameters } = event;
